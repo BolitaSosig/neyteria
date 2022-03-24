628bbb100756b41dbe87ee1ae83b14223736d2e3
dagarpa4 <82262742+dagarpa4@users.noreply.github.com>
2022-03-25 00:27:04 +0100

    HUD

---
+++ Assets/Scripts/Enemy1Controller.cs
@@ -0,0 +1,91 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy1Controller : MonoBehaviour
+{
+    // CONSTANTES
+    private const float SPEED_MOV = 2f;
+    private const float JUMP_FORCE = 15f;
+
+    // ATRIBUTOS PERSONAJE
+    [SerializeField] private float HP = 100f;
+    [SerializeField] private float Stamina = 100f;
+    [SerializeField] private float Attack = 1f;
+    [SerializeField] private float Defense = 1f;
+    [SerializeField] private float Weight = 1f;
+    [SerializeField] private float MovSpeed = 1f;
+    [SerializeField] private float JumpCap = 1f;
+    [SerializeField] private float mov_x = 0f;
+    [SerializeField] private int cont_mov_x = 0;
+
+    // REFERENCIAS
+    private Rigidbody2D _rigidbody2D;
+    private BoxCollider2D _boxCollider2D;
+    private Animator _animator;
+
+
+    private (Vector2, Vector2) getGroundCheckCorners()
+    {
+        Vector3 max = _boxCollider2D.bounds.max;
+        Vector3 min = _boxCollider2D.bounds.min;
+        Vector2 corner1 = new Vector2(max.x, min.y - 0.1f);
+        Vector2 corner2 = new Vector2(min.x, min.y - 0.1f);
+        return (corner1, corner2);
+    }
+    public bool grounded
+    {
+        get
+        {
+            var (corner1, corner2) = getGroundCheckCorners();
+            Collider2D hit = Physics2D.OverlapArea(corner1, corner2);
+            return (hit != null);
+        }
+    }
+
+    public bool canDash {
+        get { return Input.GetAxisRaw("Dash") == 1; }
+    }
+
+    /* Flag que devuelve True si el personaje está en el aire. */
+    public bool onAir
+    {
+        get { return _rigidbody2D.velocity.y != 0 /*&& !grounded*/; }
+    }
+
+    void Start()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+        _boxCollider2D = GetComponent<BoxCollider2D>();
+        _animator = GetComponent<Animator>();
+    }
+
+
+    void Update()
+    {
+        Moverse();
+    }
+
+    void Moverse()
+    {
+        
+
+        if (cont_mov_x > 50) {
+            cont_mov_x = 0;
+            mov_x = Random.Range(-0.25f, 0.25f);
+            if (mov_x > 0.1) { mov_x = 1; } else if (mov_x < -0.1) { mov_x = -1; } else { mov_x = 0; }
+        }
+        else { cont_mov_x++; }
+        
+        ////// HORIZONTAL //////
+        if(mov_x == 1) 
+            transform.localScale = new Vector2(1, transform.localScale.y); // mira a la derecha
+        else if (mov_x == -1) 
+            transform.localScale = new Vector2(-1, transform.localScale.y); // mira a la izquerda
+
+        _rigidbody2D.velocity = new Vector2(mov_x * SPEED_MOV * MovSpeed, _rigidbody2D.velocity.y); // desplazamiento del personaje
+        _animator.SetFloat("velocity_x", Mathf.Abs(_rigidbody2D.velocity.x)); // establece velocity_x en el animator
+
+    }
+
+}
