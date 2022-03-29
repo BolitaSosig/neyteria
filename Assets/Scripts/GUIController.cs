a7fc757e78b5272977de9963252f975fe9072b75
dagarpa4 <82262742+dagarpa4@users.noreply.github.com>
2022-03-29 22:28:02 +0200

    He metido assets y he cambiado el HUD
    mírate un poco los assets, hay algunos que son maravillas. En itchio está petao de gente generosa.

--- Assets/Scripts/GUIController.cs
+++ Assets/Scripts/GUIController.cs
@@ -10,9 +10,35 @@ public class GUIController : MonoBehaviour
     [SerializeField] private GameObject staminaBar;
     [SerializeField] private GameObject staminaBarRelleno;
 
+    //Escala de HealtBar y su relleno
+    float HBscaleY;
+    float HBscaleX;
+    float HBRscaleY;
+    float HBRscaleX;
+
+    //Escala de StamineBar y su relleno
+    float SBscaleY;
+    float SBscaleX;
+    float SBRscaleY;
+    float SBRscaleX;
+
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
+
+        //Escala de HealtBar y su relleno
+        HBscaleY = healthBar.transform.localScale.y;
+        HBscaleX = healthBar.transform.localScale.x;
+
+        HBRscaleY = healthBarRelleno.transform.localScale.y;
+        HBRscaleX = healthBarRelleno.transform.localScale.x;
+
+        //Escala de StamineBar y su relleno
+        SBscaleY = staminaBar.transform.localScale.y;
+        SBscaleX = staminaBar.transform.localScale.x;
+
+        SBRscaleY = staminaBarRelleno.transform.localScale.y;
+        SBRscaleX = staminaBarRelleno.transform.localScale.x;
     }
 
     void Update()
@@ -23,13 +49,13 @@ public class GUIController : MonoBehaviour
 
     void UpdateHealthBar()
     {
-        healthBar.transform.localScale = new Vector2(player.MaxHP / 100f, 1);
-        healthBarRelleno.transform.localScale = new Vector2(player.HP / 100f, 1);
+        healthBar.transform.localScale = new Vector2(HBscaleX *(player.MaxHP / 100f), HBscaleY);
+        healthBarRelleno.transform.localScale = new Vector2(HBRscaleX * (player.HP / 100f), HBRscaleY);
     }
 
     void UpdateStaminaBar()
     {
-        staminaBar.transform.localScale = new Vector2(player.MaxStamina / 100f, 1);
-        staminaBarRelleno.transform.localScale = new Vector2(player.Stamina / 100f, 1);
+        staminaBar.transform.localScale = new Vector2(SBscaleX * (player.MaxStamina / 100f), SBscaleY);
+        staminaBarRelleno.transform.localScale = new Vector2(SBRscaleX * (player.Stamina / 100f), SBRscaleY);
     }
 }
---
+++ Assets/Sprites/Descargados/Crafting Game Assets/README.txt
@@ -0,0 +1,12 @@
+Template created by Piranha305 
+YouTube : https://www.youtube.com/user/armandoalonso305/videos
+Twitter : https://twitter.com/piranha_305
+Itch.io : https://piranha305.itch.io/
+Github : https://github.com/armandoalonso
+
+Original Assets used
+
+https://0x72.itch.io/16x16-dungeon-tileset
+https://superdark.itch.io/enchanted-forest-characters
+https://ggbot.itch.io/quinquefive-font
+
