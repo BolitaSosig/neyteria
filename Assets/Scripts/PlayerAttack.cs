18b04c24569accc4ebf84ac9fefbbd970984790d
dagarpa4 <82262742+dagarpa4@users.noreply.github.com>
2022-04-12 02:48:05 +0200

    Sistema de Combate (con algunas animaciones)

--- Assets/Scripts/PlayerAttack.cs
+++ Assets/Scripts/PlayerAttack.cs
@@ -8,6 +8,12 @@ public class PlayerAttack : MonoBehaviour
     //Referencias
     private Animator _animator;
 
+    //Selector
+    public int seleccionado = 2; //0 es pistola, 1 es espada, y 2 es maza
+    GameObject _gun;
+    GameObject _sword;
+    GameObject _maze;
+
     //Gun
     private Transform _shootTransform;
     public GameObject bullet;
@@ -19,22 +25,29 @@ public class PlayerAttack : MonoBehaviour
     private Transform _swordTransform;
     public LayerMask enemyLayers;
     public float swordRange = 0.5f;
-    public float swordRate = 0.3f;
+    public float swordRate = 0.4f;
     public float swordLastTime = 0;
 
     //Maza
     private Transform _mazeTransform;
-    public float mazeRange = 0.7f;
-    public float mazedRate = 1f;
+    public float mazeRange = 0.8f;
+    public float mazeRate = 1f;
     public float mazeLastTime = 0;
 
     // Start is called before the first frame update
     void Start()
     {
         _animator = GetComponent<Animator>();
+
         _shootTransform = GameObject.Find("ShootTransform").transform;
         _swordTransform = GameObject.Find("SwordTransform").transform;
         _mazeTransform = GameObject.Find("MazeTransform").transform;
+
+        _gun = GameObject.Find("pistolas sci-fi_3");
+        _sword = GameObject.Find("pivote-sword");
+        _maze = GameObject.Find("pivote-maze");
+
+        UpdateSeleccionado();
     }
 
     // Update is called once per frame
@@ -43,12 +56,14 @@ public class PlayerAttack : MonoBehaviour
         GunAttack();
         SwordAttack();
         MazeAttack();
+        if (Input.GetKeyDown(KeyCode.Q)) UpdateSeleccionado();
     }
 
     void GunAttack()
     {
-        if (Input.GetButtonDown("Fire1") && Time.time > shootLastTime)
+        if (seleccionado == 0 && Input.GetButtonDown("Fire1") && Time.time > shootLastTime)
         {
+            _animator.SetTrigger("gunAttack");
             Debug.Log("Se crea bullet");
             GameObject newBullet;
             newBullet = Instantiate(bullet, _shootTransform.position, _shootTransform.rotation);
@@ -70,8 +85,10 @@ public class PlayerAttack : MonoBehaviour
     void SwordAttack()
     {
 
-        if (Input.GetButtonDown("Fire1") && Time.time > swordLastTime)
+        if (seleccionado == 1 && Input.GetButtonDown("Fire1") && Time.time > swordLastTime)
         {
+
+            _animator.SetTrigger("swordAttack");
             Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(_swordTransform.position, swordRange, enemyLayers);
 
             foreach (Collider2D enemy in hitEnemies)
@@ -82,14 +99,16 @@ public class PlayerAttack : MonoBehaviour
             Debug.Log("golpe con la espada");
 
             swordLastTime = Time.time + swordRate;
+
         }
     }
 
     void MazeAttack()
     {
 
-        if (Input.GetButtonDown("Fire1") && Time.time > mazeLastTime)
+        if (seleccionado == 2 && Input.GetButtonDown("Fire1") && Time.time > mazeLastTime)
         {
+            _animator.SetTrigger("mazeAttack");
             Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(_mazeTransform.position, mazeRange, enemyLayers);
 
             foreach (Collider2D enemy in hitEnemies)
@@ -99,7 +118,7 @@ public class PlayerAttack : MonoBehaviour
 
             Debug.Log("golpe con la espada");
 
-            swordLastTime = Time.time + swordRate;
+            mazeLastTime = Time.time + mazeRate;
         }
     }
 
@@ -109,6 +128,15 @@ public class PlayerAttack : MonoBehaviour
 
         Gizmos.DrawWireSphere(_swordTransform.position, swordRange);
         Gizmos.DrawWireSphere(_mazeTransform.position, mazeRange);
+
+    }
+
+    void UpdateSeleccionado()
+    {
+        seleccionado = (seleccionado + 1) % 3;
+        if (seleccionado == 0) { _gun.SetActive(true); _sword.SetActive(false); _maze.SetActive(false); }
+        else if (seleccionado == 1) { _gun.SetActive(false); _sword.SetActive(true); _maze.SetActive(false); }
+        else /*(seleccionado == 2)*/ { _gun.SetActive(false); _sword.SetActive(false); _maze.SetActive(true); }
 
     }
 
