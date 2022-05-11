4cdfc7ed4fae258b3d63f68d2a878d10b06777a1
BolitaSosig <82167693+BolitaSosig@users.noreply.github.com>
2022-05-11 15:39:44 +0200

    Player Items funciona
    - Terminada funcionalidad de los items del personaje principal. Ahora se pueden añadir, quitar y eliminar por completo items del inventario.

--- Assets/Scripts/Player/PlayerItems.cs
+++ Assets/Scripts/Player/PlayerItems.cs
@@ -1,20 +1,59 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class PlayerItems : MonoBehaviour
 {
-    public List<(Item item, int cant)> items;
+    private Dictionary<Item, int> items = new Dictionary<Item, int>();
+    public bool printItems = false;
 
 
-
-    public void AddItem(Item i, int c)
+    public void Start()
     {
-        items.Add((i, c));
+        Add(Resources.Load<Item>("Data\\Item\\Degiterio"), 0);
+        Add(Resources.Load<Item>("Data\\Item\\Occaterio"), 0);
     }
 
-    public void RemoveItem(Item i, int c)
+    public void Update()
     {
-        items.Remove((i, c));
+        
+    }
+
+    public void Add(Item i, int c)
+    {
+        if(items.ContainsKey(i))
+        {
+            items[i] = Mathf.Min(c + items[i], i.maxCant);
+        } else
+        {
+            items.Add(i, c);
+        }
+    }
+
+    public void Remove(Item i, int c)
+    {
+        if(items.ContainsKey(i))
+        {
+            int cant = items[i] - c;
+            if (cant >= 0)
+                items[i] = cant;
+        }
+    }
+
+    public void Remove(Item i)
+    {
+        items.Remove(i);
+    }
+
+    public void ShowConsole()
+    {
+        string res = "\n---------------------\nPlayer Items:\n";
+        foreach(Item item in items.Keys)
+        {
+            res += "  " + item.nombre + " x" + items[item] + "\n";
+        }
+        res += "---------------------\n";
+        Debug.Log(res);
     }
 }
