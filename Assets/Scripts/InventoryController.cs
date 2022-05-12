bdef89663fde7177a763ec740aaf96264e87844d
BolitaSosig <82167693+BolitaSosig@users.noreply.github.com>
2022-05-12 19:30:12 +0200

    GUI Inventario
    - Al presionar la tecla F, se abrirá el inventario.
    - Bug por el que no se mueve el personaje (revisar, me quedé sin tiempo).

---
+++ Assets/Scripts/InventoryController.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InventoryController : MonoBehaviour
+{
+    private PlayerItems _items;
+    public GameObject _itemPrefab;
+    public GameObject _viewportContent;
+
+    [SerializeField]
+    public bool isShown
+    {
+        set
+        {
+            GetComponent<Animator>().SetBool("show", value);
+        }
+        get
+        {
+            return GetComponent<Animator>().GetBool("show");
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _items = GameObject.Find("Player").GetComponent<PlayerItems>();
+        _items._inventoryController = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.F))
+        {
+            isShown = !isShown;
+        }
+    }
+
+    void FillItems()
+    {
+        DeleteItems();
+        foreach((Item i, int c) in _items.getAllItemsCant())
+        {
+            GameObject go = Instantiate(_itemPrefab);
+            go.GetComponentsInChildren<Image>()[0].sprite = i.icono;
+            go./*transform.Find("CantBack").*/GetComponentInChildren<TextMeshProUGUI>().text = c.ToString();
+            go.transform.parent = _viewportContent.transform;
+        }
+    }
+
+    void DeleteItems()
+    {
+        foreach(Transform go in _viewportContent.transform)
+        {
+            Destroy(go.gameObject);
+        }
+    }
+}
--- Assets/Scripts/Player/PlayerItems.cs
+++ Assets/Scripts/Player/PlayerItems.cs
@@ -5,29 +5,50 @@ using UnityEngine;
 
 public class PlayerItems : MonoBehaviour
 {
-    public static Item NONE = Resources.Load<Item>("Data\\Item\\NONE");
-    public static Item DEGITERIO = Resources.Load<Item>("Data\\Item\\Degiterio");
-    public static Item OCCATERIO = Resources.Load<Item>("Data\\Item\\Occaterio");
-    public static Item SUERO_VITAL = Resources.Load<Item>("Data\\Item\\Suero_vital");
-    public static Item SUERO_ENERGETICO = Resources.Load<Item>("Data\\Item\\Suero_energetico");
-    public static Item SUERO_FORTALECEDOR = Resources.Load<Item>("Data\\Item\\Suero_fortalecedor");
-    public static Item SUERO_PROTECTOR = Resources.Load<Item>("Data\\Item\\Suero_protector");
-    public static Item MINERAL_FRAGMENTADO = Resources.Load<Item>("Data\\Item\\Mineral_fragmentado");
-    public static Item NEXOTEK = Resources.Load<Item>("Data\\Item\\Nexotek");
+    public static Item NONE;
+    public static Item DEGITERIO;
+    public static Item OCCATERIO;
+    public static Item SUERO_VITAL;
+    public static Item SUERO_ENERGETICO;
+    public static Item SUERO_FORTALECEDOR;
+    public static Item SUERO_PROTECTOR;
+    public static Item MINERAL_FRAGMENTADO;
+    public static Item NEXOTEK;
 
     private Dictionary<Item, int> items = new Dictionary<Item, int>();
     public bool printItems = false;
 
+    public InventoryController _inventoryController;
+
 
     public void Start()
     {
-        Add(DEGITERIO, 0);
+        StartItems(); 
+
+        Add(DEGITERIO, 10);
         Add(OCCATERIO, 0);
+    }
+
+    void StartItems()
+    {
+        NONE = Resources.Load<Item>("Data\\Item\\NONE");
+        DEGITERIO = Resources.Load<Item>("Data\\Item\\Degiterio");
+        OCCATERIO = Resources.Load<Item>("Data\\Item\\Occaterio");
+        SUERO_VITAL = Resources.Load<Item>("Data\\Item\\Suero_vital");
+        SUERO_ENERGETICO = Resources.Load<Item>("Data\\Item\\Suero_energetico");
+        SUERO_FORTALECEDOR = Resources.Load<Item>("Data\\Item\\Suero_fortalecedor");
+        SUERO_PROTECTOR = Resources.Load<Item>("Data\\Item\\Suero_protector");
+        MINERAL_FRAGMENTADO = Resources.Load<Item>("Data\\Item\\Mineral_fragmentado");
+        NEXOTEK = Resources.Load<Item>("Data\\Item\\Nexotek");
     }
 
     public void Update()
     {
-        
+        if(printItems)
+        {
+            printItems = false;
+            ShowConsole();
+        }
     }
 
     public void Add(Item i, int c)
@@ -39,6 +60,7 @@ public class PlayerItems : MonoBehaviour
         {
             items.Add(i, c);
         }
+        _inventoryController.SendMessage("FillItems");
     }
 
     public void Remove(Item i, int c)
@@ -49,11 +71,23 @@ public class PlayerItems : MonoBehaviour
             if (cant >= 0)
                 items[i] = cant;
         }
+        _inventoryController.SendMessage("FillItems");
     }
 
     public void Remove(Item i)
     {
         items.Remove(i);
+        _inventoryController.SendMessage("FillItems");
+    }
+
+    public List<(Item, int)> getAllItemsCant()
+    {
+        List<(Item, int)> res = new List<(Item, int)>();
+        foreach(Item item in items.Keys)
+        {
+            res.Add((item, items[item]));
+        }
+        return res;
     }
 
     public void ShowConsole()
