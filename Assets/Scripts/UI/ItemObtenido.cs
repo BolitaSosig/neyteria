86a873c9e0b294645d78c2f84faba348a61f6ef0
BolitaSosig <82167693+BolitaSosig@users.noreply.github.com>
2022-06-08 17:03:05 +0200

    4 Item Obtenidos en UI

--- Assets/Scripts/UI/ItemObtenido.cs
+++ Assets/Scripts/UI/ItemObtenido.cs
@@ -6,13 +6,22 @@ using UnityEngine.UI;
 
 public class ItemObtenido : MonoBehaviour
 {
-    bool _showing = false;
+    [SerializeField] private GameObject[] obj = new GameObject[4];
     Queue<(Item, int)> itemQueue = new Queue<(Item, int)>();
 
-    bool Showing
+
+    void SetShowing(int i, bool value)
     {
-        get { return GetComponent<Animator>().GetBool("show"); }
-        set { GetComponent<Animator>().SetBool("show", value); }
+        obj[i].GetComponent<Animator>().SetBool("show", value);
+    }
+    bool GetShowing(int i)
+    {
+        return obj[i].GetComponent<Animator>().GetBool("show");
+    }
+
+    bool NoOneShowing()
+    {
+        return !GetShowing(0) && !GetShowing(1) && !GetShowing(2) && !GetShowing(3);
     }
 
     // Start is called before the first frame update
@@ -24,12 +33,15 @@ public class ItemObtenido : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(itemQueue.Count > 0)
+        if(itemQueue.Count > 0 && NoOneShowing())
         {
-            if (!Showing)
+            for(int i = 0; i < obj.Length && itemQueue.Count > 0; i++)
             {
-                (Item item, int cant) pair = itemQueue.Dequeue();
-                StartCoroutine(ShowItemTimer(pair.item, pair.cant));
+                if(!GetShowing(i))
+                {
+                    (Item item, int cant) pair = itemQueue.Dequeue();
+                    StartCoroutine(ShowItemTimer(i, pair.item, pair.cant));
+                }
             }
         }
     }
@@ -40,13 +52,13 @@ public class ItemObtenido : MonoBehaviour
         itemQueue.Enqueue((item, cant));
     }
 
-    IEnumerator ShowItemTimer(Item item, int cant)
+    IEnumerator ShowItemTimer(int i, Item item, int cant)
     {
-        Showing = true;
+        SetShowing(i, true);
         yield return new WaitForSecondsRealtime(0.2f);
-        GetComponentsInChildren<Image>()[1].sprite = item.icono;
-        GetComponentInChildren<TextMeshProUGUI>().text = "x" + cant + " " + item.nombre;
+        obj[i].GetComponentsInChildren<Image>()[1].sprite = item.icono;
+        obj[i].GetComponentInChildren<TextMeshProUGUI>().text = "x" + cant + " " + item.nombre;
         yield return new WaitForSecondsRealtime(3f);
-        Showing = false;
+        SetShowing(i, false);
     }
 }
