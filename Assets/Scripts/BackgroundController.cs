3909220ecb7fbd7246d545c48f88eae47d19d1ee
BolitaSosig <82167693+BolitaSosig@users.noreply.github.com>
2022-06-14 14:22:47 +0200

    ParallaxEffect Background Implemetado

--- Assets/Scripts/BackgroundController.cs
+++ Assets/Scripts/BackgroundController.cs
@@ -6,7 +6,12 @@ public class BackgroundController : MonoBehaviour
 {
     [SerializeField] private CameraController camera;
     [SerializeField] private Transform pointer;
-    public bool active = false;
+    public PolygonCollider2D zone_collider;
+
+    public bool active 
+    { 
+        get { return GLOBAL.zona.Equals(zone_collider.name); }
+    }
 
 
     private Vector3 prevPos;
@@ -15,28 +20,36 @@ public class BackgroundController : MonoBehaviour
     void Start()
     {
         camera = FindObjectOfType<CameraController>();
+        prevPos = pointer.position;
     }
 
     // Update is called once per frame
     void LateUpdate()
     {
         if (active)
-            Active2();
+            Active();
+        else 
+        {
+            foreach (Transform t in transform)
+            {
+                t.position = new Vector3(t.parent.position.x, t.parent.position.y, t.position.z);
+            }
+        }
     }
 
     void Active()
     {
-        float x = 2 * pointer.position.x - camera.transform.position.x;
-        float y = 2 * pointer.position.y - camera.transform.position.y;
-        Debug.Log("(" + x + ", " + y + ")");
+        float deltaX = camera.transform.position.x - prevPos.x;
+        float deltaY = camera.transform.position.y - prevPos.y;
         foreach (Transform t in transform)
         {
-            float z = t.position.z;
-            t.position = new Vector3(x + z, y + z, z);
+            float v = t.position.z / transform.childCount;
+            t.Translate(new Vector3(v * deltaX, v * deltaY, 0));
         }
+        prevPos = camera.transform.position;
     }
 
-    void Active2()
+    void Deactive()
     {
 
     }
--- Assets/Scripts/SceneController.cs
+++ Assets/Scripts/SceneController.cs
@@ -7,7 +7,7 @@ public class SceneController : MonoBehaviour
 {
     public SpriteRenderer background;
     
-    [SerializeField] private Sprite[] background_sprites;
+    [SerializeField] private BackgroundController[] background_sprites;
     [SerializeField] private string[] background_names = new string[] {
         "CollisionNivel1",
         "CollisionNivel2",
@@ -78,70 +78,65 @@ public class SceneController : MonoBehaviour
         switch(name)
         {
             case "CollisionNivel1":
-                CollisionNivel1();
+                CollisionNivel1(name);
                 break;
             case "CollisionNivel2":
-                CollisionNivel2();
+                CollisionNivel2(name);
                 break;
             case "CollisionNivel31Under":
-                CollisionNivel31Under();
+                CollisionNivel31Under(name);
                 break;
             case "CollisionNivel31Outer":
-                CollisionNivel31Outer();
+                CollisionNivel31Outer(name);
                 break;
             case "CollisionNivel32":
-                CollisionNivel32();
+                CollisionNivel32(name);
                 break;
         }
     }
 
-    void CollisionNivel1()
+    void CollisionNivel1(string name)
     {
-        GLOBAL.zona =  background_names[0];
+        GLOBAL.zona =  name;
         Luz luz = GameObject.Find("Luz").GetComponent<Luz>();
         luz._linterna.color = new Color(1f, 1f, 0.6f);
         luz._globalLight.color = new Color(0f, 0f, 0f);
         luz._globalLight.intensity = 1f;
-        background.sprite = background_sprites[0];
     }
 
-    void CollisionNivel2()
+    void CollisionNivel2(string name)
     {
-        GLOBAL.zona = background_names[1];
+        GLOBAL.zona = name;
         Luz luz = GameObject.Find("Luz").GetComponent<Luz>();
         luz._linterna.color = new Color(0f, 0f, 0f);
         luz._globalLight.color = new Color(0.5f, 0.4f, 0.2f);
         luz._globalLight.intensity = 1f;
-        background.sprite = background_sprites[1];
     }
 
-    void CollisionNivel31Under()
+    void CollisionNivel31Under(string name)
     {
-        GLOBAL.zona = background_names[2];
+        GLOBAL.zona = name;
         Luz luz = GameObject.Find("Luz").GetComponent<Luz>();
         luz._linterna.color = new Color(0f, 0f, 0f);
         luz._globalLight.color = new Color(0.5f, 0.4f, 0.2f);
         luz._globalLight.intensity = 0.5f;
-        background.sprite = background_sprites[2];
     }
 
-    void CollisionNivel31Outer()
+    void CollisionNivel31Outer(string name)
     {
-        GLOBAL.zona = background_names[3];
+        GLOBAL.zona = name;
         Luz luz = GameObject.Find("Luz").GetComponent<Luz>();
         luz._linterna.color = new Color(0f, 0f, 0f);
         luz._globalLight.color = new Color(1f, 1f, 1f);
         luz._globalLight.intensity = 2f;
-        background.sprite = background_sprites[3];
     }
 
-    void CollisionNivel32()
+    void CollisionNivel32(string name)
     {
-        GLOBAL.zona = background_names[4];
+        GLOBAL.zona = name;
         Luz luz = GameObject.Find("Luz").GetComponent<Luz>();
         luz._linterna.color = new Color(0f, 0f, 0f);
         luz._globalLight.color = new Color(0.7f, 1f, 0.6f);
         luz._globalLight.intensity = 1f;
-        background.sprite = background_sprites[4];
     }
 }
